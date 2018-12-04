fbd9a67cf0bd7db076434323f04fd357d55e3bb2
dev1 <dev1@gmail.com>
2018-12-04 17:36:57 +0530

    mody a1.java

--- a1.java
+++ a1.java
@@ -6,3 +6,13 @@ system.out.println("a1 by dev1");
 } #eof by dev1
 =======================
 output: a1
+
+===============================================
+ # hello world
+ 
+{
+	public static void main () # by dev1
+	{
+		system.out.println ("hello world");
+	}
+}# eof by dev1
